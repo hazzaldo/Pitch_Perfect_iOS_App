3376f5b8e3584495e8f25dbf28342c31f587d96c
hazzaldo <hazzaldo@hotmail.com>
2016-01-05 03:24:25 +0200

    creation of Pitch Perfect App project

---
+++ Pitch_Perfect/Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Hareth Naji on 05/01/2016.
+//  Copyright © 2016 Hazzaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
