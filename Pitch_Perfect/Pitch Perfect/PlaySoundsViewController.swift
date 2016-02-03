2eaba446ec25302f777d396aaabec268d07d033e
hazzaldo <hazzaldo@hotmail.com>
2016-02-04 00:42:13 +0200

    Add slow button and code to play sound file - compiled successfully

--- Pitch_Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch_Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -7,23 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer:AVAudioPlayer!
 
 class PlaySoundsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let string_path = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
+        let url_path = NSURL.fileURLWithPath(string_path!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: url_path)
+            audioPlayer.enableRate = true
+        } catch {
+            print("No sound file found. Please check directory where sound file is expected and file type")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func playSlowAudio(sender: UIButton) {
-        
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+        print("Play Slow Audio")
     }
+}
 
     /*
     // MARK: - Navigation
@@ -35,4 +48,4 @@ class PlaySoundsViewController: UIViewController {
     }
     */
 
-}
+
